3b511042bda236f6f7160d05b2d8658075a8fc0e
Snurnitsyn <sergey.snurnitsyn@t-systems.com>
2022-08-31 11:41:53 +0300

    some changes

--- src/test/java/web/findby/page/LoginPage.java
+++ src/test/java/web/findby/page/LoginPage.java
@@ -6,11 +6,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import web.findby.elements.Button;
 import web.findby.elements.EditBox;
 import web.findby.elements.IFrame;
-import web.simple.config.WebConfig;
 
 public class LoginPage extends BasePage {
     @FindBy(xpath = "//*[text()='Вход']")
